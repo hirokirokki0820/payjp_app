13749f0b40b42610f5ae80b95534aac71115ad09
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-06-22 12:30:41 +0900

    Add products info (model, controller, index)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_21_132208) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_22_014515) do
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+products = [
+  { name: '鉛筆', price: 100 },
+  { name: 'ボールペン', price: 200 },
+  { name: '消しゴム', price: 50 },
+  { name: 'ノート', price: 100 }
+]
+Product.create(products)
